cda7d854892788e0707b1b8a8361bb040209acd1
MarjanGit <mansar16@huntersoe.org>
2019-07-21 11:22:58 -0400

    added base 5 issues files

---
+++ src/components/Education.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/education.css";
+
+class Education extends Component {
+  state = {};
+  render() {
+    return <div>Education</div>;
+  }
+}
+
+export default Education;
---
+++ src/components/Prostitution.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/prostitution.css";
+
+class Prostitution extends Component {
+  state = {};
+  render() {
+    return <div>Prostitution</div>;
+  }
+}
+
+export default Prostitution;
