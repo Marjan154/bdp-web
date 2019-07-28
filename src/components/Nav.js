9ed2f20ed8cf3af069d1a79313f3f43615c4352a
MarjanGit <mansar16@huntersoe.org>
2019-07-28 17:27:59 -0400

    added visibility, looks same

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Container,
   Divider,
@@ -8,41 +8,60 @@ import {
   Image,
   List,
   Menu,
-  Segment
+  Segment,
+  Visibility
 } from "semantic-ui-react";
 import DropdownNav from "./Dropdown";
 import { Link } from "react-router-dom";
 
-const FixedMenuLayout = () => (
-  <div style={{ marginBottom: "5em" }}>
-    <Segment>
-      <Menu fixed="top" inverted style={{ background: "rgb(165, 18, 18)" }}>
-        <Container>
-          <Menu.Item as="a" header style={{ padding: "25px" }}>
-            <Image
-              size="tiny"
-              src={require("../images/logoBDP.png")}
-              style={{ marginRight: "1.5em" }}
-            />
-            Bangladesh Development Project
-          </Menu.Item>
-          <Menu.Item as={Link} to="/">
-            Home
-          </Menu.Item>
-          <DropdownNav />
-          <Menu.Item as={Link} to="/projects">
-            Projects
-          </Menu.Item>
-          <Menu.Item as={Link} to="/donate">
-            Donate
-          </Menu.Item>
-          <Menu.Item as={Link} to="/team">
-            Team
-          </Menu.Item>
-        </Container>
-      </Menu>
-    </Segment>
-  </div>
-);
+class Nav extends Component {
+  state = {};
 
-export default FixedMenuLayout;
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
+  render() {
+    return (
+      <div style={{ marginBottom: "5em" }}>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Segment>
+            <Menu
+              fixed="top"
+              inverted
+              style={{ background: "rgb(165, 18, 18)" }}
+            >
+              <Container>
+                <Menu.Item as="a" header style={{ padding: "25px" }}>
+                  <Image
+                    size="tiny"
+                    src={require("../images/logoBDP.png")}
+                    style={{ marginRight: "1.5em" }}
+                  />
+                  Bangladesh Development Project
+                </Menu.Item>
+                <Menu.Item as={Link} to="/">
+                  Home
+                </Menu.Item>
+                <DropdownNav />
+                <Menu.Item as={Link} to="/projects">
+                  Projects
+                </Menu.Item>
+                <Menu.Item as={Link} to="/donate">
+                  Donate
+                </Menu.Item>
+                <Menu.Item as={Link} to="/team">
+                  Team
+                </Menu.Item>
+              </Container>
+            </Menu>
+          </Segment>
+        </Visibility>
+      </div>
+    );
+  }
+}
+
+export default Nav;
