735cd875044fb7d9bbe7738623763cf3d942496d
yitongni <31358391+yitongni@users.noreply.github.com>
2019-07-28 20:11:57 -0400

    refractor Slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,70 +1,61 @@
-// import React, { Component } from "react";
-// // import Carousel from "react-bootstrap/Carousel";
-// import Carousel from "react-bootstrap-carousel";
-// import "bootstrap/dist/css/bootstrap.css";
-// import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
+import React, { Component } from "react";
+import Carousel from "semantic-ui-carousel-react";
+import {Image} from "semantic-ui-react";
 
-// class Slideshow extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
 
-//   //   ControlledCarousel() {
-//   //     const [index, setIndex] = useState(0);
-//   //     const [direction, setDirection] = useState(null);
+class Slideshow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-//   //     const handleSelect = (selectedIndex, e) => {
-//   //       setIndex(selectedIndex);
-//   //       setDirection(e.direction);
-//   //     };
-//   //   }
+  render() {
+    const elements = [
+        {
+          render: () => {
+            return (
+              <Image
+                src={require("../images/1.jpg")}
+                style={{ margin: "auto" }}
+              />
+            );
+          }
+        },
+        {
+          render: () => {
+            return (
+              <Image
+                src={require("../images/2.jpg")}
+                style={{ margin: "auto" }}
+              />
+            );
+          }
+        },
+        {
+          render: () => {
+            return (
+              <Image
+                src={require("../images/3.jpg")}
+                style={{ margin: "auto" }}
+              />
+            );
+          }
+        }
+      ];
+      
+    return (
 
-//   render() {
-//     return (
-//       <div>
-//         <Carousel>
-//           <Carousel.Item>
-//             <img
-//               className="d-block w-100"
-//               src="holder.js/800x400?text=First slide&bg=373940"
-//               alt="First slide"
-//             />
-//             <Carousel.Caption>
-//               <h3>First slide label</h3>
-//               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-//             </Carousel.Caption>
-//           </Carousel.Item>
-//           <Carousel.Item>
-//             <img
-//               className="d-block w-100"
-//               src="holder.js/800x400?text=Second slide&bg=282c34"
-//               alt="Third slide"
-//             />
+        <div style={{ width: "60%", margin: "0 auto" }}>
+            <Carousel
+                elements={elements}
+                duration={3000}
+                animation="slide left"
+                // showNextPrev={false}
+                showIndicators={true}
+            />
+        </div>
+    );
+  }
+}
 
-//             <Carousel.Caption>
-//               <h3>Second slide label</h3>
-//               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-//             </Carousel.Caption>
-//           </Carousel.Item>
-//           <Carousel.Item>
-//             <img
-//               className="d-block w-100"
-//               src="holder.js/800x400?text=Third slide&bg=20232a"
-//               alt="Third slide"
-//             />
-
-//             <Carousel.Caption>
-//               <h3>Third slide label</h3>
-//               <p>
-//                 Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-//               </p>
-//             </Carousel.Caption>
-//           </Carousel.Item>
-//         </Carousel>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Slideshow;
+export default Slideshow;
