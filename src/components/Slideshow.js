898042161326ce6d8cc9e83739d4ddc91801c6a4
yitongni <31358391+yitongni@users.noreply.github.com>
2019-08-02 23:32:56 -0400

    fixing slide show

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,46 +1,77 @@
 import React, { Component } from "react";
 import Carousel from "semantic-ui-carousel-react";
 import { Image } from "semantic-ui-react";
+// import * as fs from 'fs';
 
 class Slideshow extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      ramadanImage: [
+        { 
+          img: "1.jpg"
+        },
+        {
+          img: "2.jpg"
+        },
+        {
+          img: "3.jpg"
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    console.log(this.state.ramadanImage);
+    // console.log(files);
   }
 
   render() {
-    const elements = [
+    const elements = this.state.ramadanImage.map(member=>{
+      console.log(member.img);
       {
-        render: () => {
-          return (
-            <Image
-              src={require("../images/1.jpg")}
-              style={{ margin: "auto" }}
-            />
-          );
-        }
-      },
-      {
-        render: () => {
-          return (
-            <Image
-              src={require("../images/2.jpg")}
-              style={{ margin: "auto" }}
-            />
-          );
-        }
-      },
-      {
-        render: () => {
-          return (
-            <Image
-              src={require("../images/3.jpg")}
-              style={{ margin: "auto" }}
-            />
-          );
-        }
+      render: ()=>{
+        return (
+          <Image
+            src={require(`../images/ramadan/${member.img}`)}
+            style={{ margin: "auto" }}
+          />
+        );
       }
-    ];
+          
+      }});
+      // let  elements  = [
+      // {
+      //   render: () => {
+      //     return (
+      //       <Image
+      //         src={require("../images/1.jpg")}
+      //         style={{ margin: "auto" }}
+      //       />
+      //     );
+      //   }
+      // },
+      // {
+      //   render: () => {
+      //     return (
+      //       <Image
+      //         src={require("../images/2.jpg")}
+      //         style={{ margin: "auto" }}
+      //       />
+      //     );
+      //   }
+      // },
+      // {
+      //   render: () => {
+      //     return (
+      //       <Image
+      //         src={require("../images/3.jpg")}
+      //         style={{ margin: "auto" }}
+      //       />
+      //     );
+      //   }
+      // }
+    // ];
 
     return (
       <div style={{ width: "60%", margin: "0 auto", padding: "100px" }}>
