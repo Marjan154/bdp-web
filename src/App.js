09a8ce021f064593ebde80d223f3476b0a87a0b5
MarjanGit <mansar16@huntersoe.org>
2019-09-25 11:04:44 -0400

    added margin

--- src/App.js
+++ src/App.js
@@ -23,29 +23,25 @@ function App() {
       <Router>
         <Nav isArticle={isArticle} />
         <ScrollToTop>
-          <div>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/donate" component={Donate} />
-            <Route exact path="/issues" component={Issues} />
-            <Route exact path="/projects" component={Projects} />
-            <Route exact path="/issues/healthcare" component={Healthcare} />
-            <Route exact path="/issues/childlabor" component={ChildLabor} />
-            <Route exact path="/issues/prostitution" component={Prositution} />
-            <Route exact path="/issues/education" component={Education} />
-            <Route
-              exact
-              path="/issues/earlymarriage"
-              component={EarlyMarriage}
-            />
-            <Route
-              exact
-              path="/issues/articles/:issuetype/:articleid"
-              component={ArticlesTemplate}
-            />
-            <Route exact path="/team" component={Team} />
-            <Route exact path="/nav" component={Nav} />
-            <Route exact path="/articles" component={ArticlesTemplate} />
-          </div>
+          {/* <div> */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/donate" component={Donate} />
+          <Route exact path="/issues" component={Issues} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/issues/healthcare" component={Healthcare} />
+          <Route exact path="/issues/childlabor" component={ChildLabor} />
+          <Route exact path="/issues/prostitution" component={Prositution} />
+          <Route exact path="/issues/education" component={Education} />
+          <Route exact path="/issues/earlymarriage" component={EarlyMarriage} />
+          <Route
+            exact
+            path="/issues/articles/:issuetype/:articleid"
+            component={ArticlesTemplate}
+          />
+          <Route exact path="/team" component={Team} />
+          <Route exact path="/nav" component={Nav} />
+          <Route exact path="/articles" component={ArticlesTemplate} />
+          {/* </div> */}
         </ScrollToTop>
         <Footer />
       </Router>
