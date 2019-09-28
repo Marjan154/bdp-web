97da9249a2584371faac23c70a982c52dcddf0ca
MarjanGit <mansar16@huntersoe.org>
2019-09-28 14:41:40 -0400

    removed margin

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -30,7 +30,7 @@ class Nav extends Component {
     return (
       <div>
         <Responsive minWidth={768}>
-          <div class="nav" style={{ marginBottom: "5em" }}>
+          <div class="nav">
             <Visibility
               once={false}
               onBottomPassed={this.showFixedMenu}
