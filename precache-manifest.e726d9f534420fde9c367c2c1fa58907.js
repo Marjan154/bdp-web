38b03963e5c759c3ac6785d29d5b071dfc6ad733
MarjanGit <mansar16@huntersoe.org>
2019-09-29 13:11:51 -0400

    Updates

---
+++ precache-manifest.e726d9f534420fde9c367c2c1fa58907.js
@@ -0,0 +1,346 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b2695d407e640d41570a9e3008b8a1",
+    "url": "/bdp-web/index.html"
+  },
+  {
+    "revision": "3b2b37f75f10980d1aec",
+    "url": "/bdp-web/static/css/17.0de12420.chunk.css"
+  },
+  {
+    "revision": "04137b8cd036cbeac956",
+    "url": "/bdp-web/static/css/main.694a8d28.chunk.css"
+  },
+  {
+    "revision": "a056a36341ee9ac1d1dc",
+    "url": "/bdp-web/static/js/0.f6d764e1.chunk.js"
+  },
+  {
+    "revision": "f7d842de800481a93955",
+    "url": "/bdp-web/static/js/1.7f6bea28.chunk.js"
+  },
+  {
+    "revision": "3752ea54e1cd8e486662",
+    "url": "/bdp-web/static/js/10.cc2aecfb.chunk.js"
+  },
+  {
+    "revision": "b69eb34b8c19c17e9e74",
+    "url": "/bdp-web/static/js/11.981e3bde.chunk.js"
+  },
+  {
+    "revision": "f61f1497f16388a3355f",
+    "url": "/bdp-web/static/js/12.9a9651a1.chunk.js"
+  },
+  {
+    "revision": "8fbe0cfe7077e3b65ef6",
+    "url": "/bdp-web/static/js/13.96d32fa9.chunk.js"
+  },
+  {
+    "revision": "5da75d8f9595a6bdf85f",
+    "url": "/bdp-web/static/js/14.9b678974.chunk.js"
+  },
+  {
+    "revision": "3b2b37f75f10980d1aec",
+    "url": "/bdp-web/static/js/17.a300113f.chunk.js"
+  },
+  {
+    "revision": "4b3ac24e3c36d75224e1",
+    "url": "/bdp-web/static/js/2.a20744f1.chunk.js"
+  },
+  {
+    "revision": "cd69a518d54af3786f94",
+    "url": "/bdp-web/static/js/3.d2dcaa6e.chunk.js"
+  },
+  {
+    "revision": "7cc6e38958b9580853f2",
+    "url": "/bdp-web/static/js/4.409ee726.chunk.js"
+  },
+  {
+    "revision": "c5df023acaa09e87afa7",
+    "url": "/bdp-web/static/js/5.f295486c.chunk.js"
+  },
+  {
+    "revision": "1b2a5aa56bbe39861eb7",
+    "url": "/bdp-web/static/js/6.82cdf712.chunk.js"
+  },
+  {
+    "revision": "7018e019fa8e0c089c0b",
+    "url": "/bdp-web/static/js/7.4b238b6b.chunk.js"
+  },
+  {
+    "revision": "0b69490875983ca5b62c",
+    "url": "/bdp-web/static/js/8.8161927a.chunk.js"
+  },
+  {
+    "revision": "51406f27035991483b71",
+    "url": "/bdp-web/static/js/9.6b267fe1.chunk.js"
+  },
+  {
+    "revision": "04137b8cd036cbeac956",
+    "url": "/bdp-web/static/js/main.b934a5f3.chunk.js"
+  },
+  {
+    "revision": "f960bd87a9002d2183cf",
+    "url": "/bdp-web/static/js/runtime~main.6669b2c4.js"
+  },
+  {
+    "revision": "6197f2ad1229c2b844a1a7cb8653c5a8",
+    "url": "/bdp-web/static/media/1.6197f2ad.jpg"
+  },
+  {
+    "revision": "9ba8e445903f00532981de83ecc17dfb",
+    "url": "/bdp-web/static/media/2.9ba8e445.jpg"
+  },
+  {
+    "revision": "67928aeaa49eb7542452f0dfcdf40c64",
+    "url": "/bdp-web/static/media/3.67928aea.jpg"
+  },
+  {
+    "revision": "f0469e0b6416c1875ff78178e3515e3a",
+    "url": "/bdp-web/static/media/4.f0469e0b.jpg"
+  },
+  {
+    "revision": "3acaddc4cb558056dd0d23d3e88f7315",
+    "url": "/bdp-web/static/media/5.3acaddc4.jpg"
+  },
+  {
+    "revision": "cbfac36105b7ed6764bae1a0b8670944",
+    "url": "/bdp-web/static/media/6.cbfac361.jpg"
+  },
+  {
+    "revision": "160067dcc672a4ec93beac0f5fcf2a6d",
+    "url": "/bdp-web/static/media/7.160067dc.jpg"
+  },
+  {
+    "revision": "4f22866b5b6e1dffb5ff97b68120873c",
+    "url": "/bdp-web/static/media/Amina.4f22866b.jpg"
+  },
+  {
+    "revision": "c2924927fc0f7b7b77144577b81a4f82",
+    "url": "/bdp-web/static/media/Ashabul.c2924927.jpg"
+  },
+  {
+    "revision": "260e74b7270ce21716c2c564d06c2e68",
+    "url": "/bdp-web/static/media/Ferdous.260e74b7.jpg"
+  },
+  {
+    "revision": "8ef703e265d10bd10a65ca12ad521acd",
+    "url": "/bdp-web/static/media/Jaheen.8ef703e2.JPG"
+  },
+  {
+    "revision": "f7b9fa3693ec730cf0e34f35b79c695c",
+    "url": "/bdp-web/static/media/Karima.f7b9fa36.jpg"
+  },
+  {
+    "revision": "a5806ee828b2cdf3e7eef9cc77f50847",
+    "url": "/bdp-web/static/media/MainLabor.a5806ee8.jpg"
+  },
+  {
+    "revision": "039784e4a0eb9065d901d4973b480380",
+    "url": "/bdp-web/static/media/MainMarriage.039784e4.jpg"
+  },
+  {
+    "revision": "d1a1e97622783d36b454d0c236af8372",
+    "url": "/bdp-web/static/media/Marjan.d1a1e976.jpg"
+  },
+  {
+    "revision": "0fe0329ecab40a9585f7e32b6abb2ecb",
+    "url": "/bdp-web/static/media/Momo.0fe0329e.jpg"
+  },
+  {
+    "revision": "221a82665ba93559def6efc3d639ee7e",
+    "url": "/bdp-web/static/media/Mosammat.221a8266.jpg"
+  },
+  {
+    "revision": "7f65a86611b38c06adf7424a2547e877",
+    "url": "/bdp-web/static/media/Naiful.7f65a866.jpg"
+  },
+  {
+    "revision": "dde12879aa4048c8e419725b5478d02d",
+    "url": "/bdp-web/static/media/Noshin.dde12879.jpg"
+  },
+  {
+    "revision": "ded0ab0f5bfc2f1d7bfac81c00f33bbd",
+    "url": "/bdp-web/static/media/Prema.ded0ab0f.jpg"
+  },
+  {
+    "revision": "302b1e301ae383e3749e804dc406a030",
+    "url": "/bdp-web/static/media/Priya.302b1e30.jpg"
+  },
+  {
+    "revision": "267af63f7bc999c88cfa88896852f2d2",
+    "url": "/bdp-web/static/media/Russell-Watkins.267af63f.jpg"
+  },
+  {
+    "revision": "604f18d6d82be4467f98b41ad7b17838",
+    "url": "/bdp-web/static/media/Sila.604f18d6.jpg"
+  },
+  {
+    "revision": "160b2456d0e819f6f7c5b653cd10fdf2",
+    "url": "/bdp-web/static/media/Simran.160b2456.jpg"
+  },
+  {
+    "revision": "e4982de30cdc05b9367a0348a49c26fc",
+    "url": "/bdp-web/static/media/Tahmid.e4982de3.jpg"
+  },
+  {
+    "revision": "33d8e4d8411fba4c4aa305727bbd3c06",
+    "url": "/bdp-web/static/media/Tanjeela.33d8e4d8.jpg"
+  },
+  {
+    "revision": "e748f6f451455f9b35b61689c4fb0e1c",
+    "url": "/bdp-web/static/media/bdphand.e748f6f4.png"
+  },
+  {
+    "revision": "080396d3b3fbb2375d08461c3b6ebe50",
+    "url": "/bdp-web/static/media/bdplogo.080396d3.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/bdp-web/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/bdp-web/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/bdp-web/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/bdp-web/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/bdp-web/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "4660a10cca15cc019414131cd38bf198",
+    "url": "/bdp-web/static/media/educationArticle1.4660a10c.jpg"
+  },
+  {
+    "revision": "005db5f7f2116fe92e52bc02fb29d40f",
+    "url": "/bdp-web/static/media/educationArticle2.005db5f7.jpg"
+  },
+  {
+    "revision": "81a172f366052ee117873c5fe3db5c35",
+    "url": "/bdp-web/static/media/educationArticle3.81a172f3.jpg"
+  },
+  {
+    "revision": "a8d60ca9b3e4749a0cc8eb8ee228c19c",
+    "url": "/bdp-web/static/media/educationmain.a8d60ca9.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bdp-web/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "3abadec56a2588142ff72a3b14fe414b",
+    "url": "/bdp-web/static/media/healthArticle1.3abadec5.jpg"
+  },
+  {
+    "revision": "a4784d5511fb10af2972e29536451229",
+    "url": "/bdp-web/static/media/healthArticle2.a4784d55.jpg"
+  },
+  {
+    "revision": "e63d98346b423d818acc05bffe3a0002",
+    "url": "/bdp-web/static/media/healthcard1.e63d9834.jpg"
+  },
+  {
+    "revision": "c98d0fe5603ea9f7ee15c0cc064c1073",
+    "url": "/bdp-web/static/media/home.c98d0fe5.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/bdp-web/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/bdp-web/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/bdp-web/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/bdp-web/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/bdp-web/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "0364037b0343f6a673aae975968c7d8a",
+    "url": "/bdp-web/static/media/laborArticle1.0364037b.jpg"
+  },
+  {
+    "revision": "4468c3861066fcc7b8aa96742e00ff1a",
+    "url": "/bdp-web/static/media/laborArticle2.4468c386.jpg"
+  },
+  {
+    "revision": "aab63de41f24c47793391b41e814d2a4",
+    "url": "/bdp-web/static/media/laborArticle3.aab63de4.jpg"
+  },
+  {
+    "revision": "50b0428967858b052611757f70dcb4e9",
+    "url": "/bdp-web/static/media/logoBDP.50b04289.png"
+  },
+  {
+    "revision": "3193f6e2cb7a6a86847060d5ae6b4a31",
+    "url": "/bdp-web/static/media/mainProstitution.3193f6e2.jpg"
+  },
+  {
+    "revision": "42c09b116180c85e9e1cc4ba7f372081",
+    "url": "/bdp-web/static/media/marriageArticle1.42c09b11.jpg"
+  },
+  {
+    "revision": "4a5e932354dbbf8c70d30b428f3bb641",
+    "url": "/bdp-web/static/media/marriageArticle2.4a5e9323.jpg"
+  },
+  {
+    "revision": "cac5116f5bfe10cac0103126f2faa539",
+    "url": "/bdp-web/static/media/marriageArticle3.cac5116f.jpg"
+  },
+  {
+    "revision": "9ddc0421af092d9bb59a84e21c8ecb9e",
+    "url": "/bdp-web/static/media/oldteam.9ddc0421.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/bdp-web/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/bdp-web/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/bdp-web/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/bdp-web/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/bdp-web/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "6009446a1ce658b09889f199b413afaa",
+    "url": "/bdp-web/static/media/pinkMarriage.6009446a.jpg"
+  },
+  {
+    "revision": "0f42d51950b4c7e8861625e2781ad748",
+    "url": "/bdp-web/static/media/prostitutionArticle1.0f42d519.jpg"
+  },
+  {
+    "revision": "16839952626efca1dd3f395160a1c19c",
+    "url": "/bdp-web/static/media/prostitutionArticle2.16839952.jpg"
+  },
+  {
+    "revision": "7842c5205f29ec0260fb7bfb9d0bc1a5",
+    "url": "/bdp-web/static/media/prostitutionArticle3.7842c520.jpg"
+  }
+]);
